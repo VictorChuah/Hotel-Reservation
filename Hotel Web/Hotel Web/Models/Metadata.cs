88a42c183bb0d0a0d10f2d33be7e37cbe0b399c9
VictorChuah <68944095+VictorChuah@users.noreply.github.com>
2020-12-03 04:17:24 +0800

    email set up, reset token donem and add user name and profile pic at top right corner

--- Hotel Web/Hotel Web/Models/Metadata.cs
+++ Hotel Web/Hotel Web/Models/Metadata.cs
@@ -137,6 +137,37 @@ namespace Hotel_Web.Models
         public string Confirm { get; set; }
     }
 
+    public class forgetPassModel
+    {
+        [Required]
+        [StringLength(20)]
+        public string Username { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        [EmailAddress]
+        [RegularExpression(@"^.+@.+mail.com$", ErrorMessage = "Invalid Email format")]
+        public string Email { get; set; }
+    }
+
+    public class ResetPassModel
+    {
+        [Display(Name = "New Password")]
+        [Required]
+        [StringLength(20, MinimumLength = 6)]
+        [RegularExpression(@"^(?=.+[a-z])(?=.+[A-Z])(?=.+\d)(?=.+[-!@$%^&*()_+|~=`{}\[\]:;'<>?,.\/\\])[A-Za-z\d-!@$%^&*()_+|~=`{}\[\]:;'<>?,.\/\\]{6,20}$",
+            ErrorMessage = "Password must be between 6 to 20, contain at least 1 lower and uppercase, a digit and a symbol")]
+        public string New { get; set; }
+
+        [Display(Name = "Confirm Password")]
+        [Required]
+        [StringLength(20, MinimumLength = 6)]
+        [RegularExpression(@"^(?=.+[a-z])(?=.+[A-Z])(?=.+\d)(?=.+[-!@$%^&*()_+|~=`{}\[\]:;'<>?,.\/\\])[A-Za-z\d-!@$%^&*()_+|~=`{}\[\]:;'<>?,.\/\\]{6,20}$",
+            ErrorMessage = "Password must be between 6 to 20, contain at least 1 lower and uppercase, a digit and a symbol")]
+        [System.ComponentModel.DataAnnotations.Compare("New")]
+        public string Confirm { get; set; }
+    }
+
     public class AdminCSEditVM
     {
         [Required]
